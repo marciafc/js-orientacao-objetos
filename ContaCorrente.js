7b5ef0d688fdb28ecc50fdbacd35a7d949761388
Marcia Castagna <marcia.castagna@hypeflame.com.br>
2021-02-15 22:29:02 -0300

    Acessores atributos privados

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,6 +1,27 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-  cliente;
   agencia;
+
+  _cliente;
+
+  // Isso funciona com a atribuição de igualdade, não chamar como um método
+  set cliente(novoValor) {
+    // -> Quando escrever Cliente, vai fazer import, mas não coloca o .js (adicionar na mão)
+    // -> Com essa validação, qlq coisa diferente de uma instância de cliente, o valor ficará undefined
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
   _saldo = 0;
 
   sacar(valor) {
--- index.js
+++ index.js
@@ -20,54 +20,26 @@ contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
 
 const valorSacado = contaCorrenteRicardo.sacar(50);
-
-console.log(contaCorrenteRicardo);
-/**
- ContaCorrente {
-  cliente: Cliente { nome: 'Ricardo', cpf: 11122233309 },
-  agencia: 1001,
-  _saldo: 150
-}
-*/
-
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-
-// Caso não instancie 'conta2.cliente = new Cliente();', irá ocorrer o erro
-// TypeError: Cannot set property 'nome' of undefined
-conta2.cliente.nome = "Alice";
-
-conta2.cliente.cpf = 88822233309;
+conta2.cliente = cliente1;
+//conta2.cliente = 0; // ContaCorrente { agencia: 102, _cliente: undefined, _saldo: 200 }
+//conta2.cliente.nome = "Alice";
+//conta2.cliente.cpf = 88822233309;
 conta2.agencia = 102;
 
-const conta3 = new ContaCorrente();
-conta3.agencia = 102;
-console.log("\nConta sem cliente - undefined");
-console.log(conta3);
-// ContaCorrente { cliente: undefined, agencia: 102, _saldo: 0 }
+console.log("\nAcessando o cliente");
+console.log(conta2.cliente);
 
-const conta4 = new ContaCorrente();
-conta4.cliente = null; // por DEFAULT é undefined, para ser null tem que dizer explicitamente
-//conta4.cliente.nome = "Nome"; // TypeError: Cannot set property 'nome' of null
-conta4.agencia = 102;
-console.log("\nConta sem cliente - null");
-console.log(conta4);
-// ContaCorrente { cliente: null, agencia: 102, _saldo: 0 }
+console.log("\nAcessando o saldo do cliente");
+console.log(conta2.saldo);
+
+// Como só há o modificador de acesso get no 'saldo', não permite fazer set
+//conta2.saldo = 100; // TypeError: Cannot set property saldo of #<ContaCorrente> which has only a getter
 
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
 
-//console.log("valor: ", valor);
-// valor:  200
-
 console.log("\n");
 console.log(conta2);
-/**
-ContaCorrente {
-  cliente: Cliente { nome: 'Alice', cpf: 88822233309 },
-  agencia: 102,
-  _saldo: 200
-}
- */
